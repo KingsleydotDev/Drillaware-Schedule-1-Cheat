f58b19bb523751ad59095481e7928778984a4575
KingsleydotDev <78937949+KingsleydotDev@users.noreply.github.com>
2025-05-08 19:30:08 +0100

    updated offsets to v0.3.5f3 changed comments to work with offsetupdater

--- Drillaware Schedule 1/offsets.cpp
+++ Drillaware Schedule 1/offsets.cpp
@@ -3,49 +3,47 @@
 #include <Windows.h>
 
 // Offsets namespace for memory-related variables
+// updated offsets to v0.3.5f3
 namespace offsets {
     uintptr_t base = (uintptr_t)GetModuleHandle(NULL);
     uintptr_t GameAssembly = (uintptr_t)GetModuleHandle("GameAssembly.dll");
 
     namespace localplayer {
-        uintptr_t CanTakeDamage = 0x6BE170; // ScheduleOne_PlayerScripts_Health_PlayerHealth$$get_CanTakeDamage proc near
-        uintptr_t TakeDamage = 0x6BD560; // ScheduleOne_PlayerScripts_Health_PlayerHealth$$RpcWriter___Observers_TakeDamage_3505310624 proc near
-        uintptr_t GetStackLimit = 0x8527B0; // ScheduleOne_ItemFramework_ItemInstance$$get_StackLimit proc near
-        uintptr_t SetStamina = 0x6C6670; // ScheduleOne_PlayerScripts_PlayerMovement$$SetStamina proc nea
-        uintptr_t UpdateArrest = 0x62DAD0; // ScheduleOne.NPCs.Behaviour.PursuitBehaviour$$UpdateArrest proc near
-        uintptr_t UpdateSearch = 0x609A90; // ScheduleOne.NPCs.Behaviour.BodySearchBehaviour$$UpdateSearch proc near
-        uintptr_t AssignTarget = 0x606E60; // ScheduleOne.NPCs.Behaviour.BodySearchBehaviour$$AssignTarget proc near
-        uintptr_t DoesPlayerContainItemsOfInterest = 0x607A70; // ScheduleOne.NPCs.Behaviour.BodySearchBehaviour$$DoesPlayerContainItemsOfInterest proc near
-        uintptr_t BeginBodySearch = 0x57BAF0; // ScheduleOne.Police.PoliceOfficer$$BeginBodySearch proc near
-        uintptr_t BeginBodySearch_LocalPlayer = 0x57B940; // ScheduleOne.Police.PoliceOfficer$$BeginBodySearch_LocalPlayer proc near
-        uintptr_t BeginBodySearch_Networked = 0x57B9C0; // ScheduleOne.Police.PoliceOfficer$$BeginBodySearch_Networked proc near
-        uintptr_t PunchControllerUpdate = 0x76F220; //ScheduleOne_Combat_PunchController$$Update proc near
-        uintptr_t SetfieldOfView = 0x2987CA0; //UnityEngine_Camera$$set_fieldOfView proc near
-        uintptr_t PlayerMovementUpdate = 0x6C78C0; // ScheduleOne.PlayerScripts.PlayerMovement$$Update
-        uintptr_t SkatingSkateboardUpdate = 0xA64BE0; // ScheduleOne_Skating_Skateboard$$Update proc near
+        uintptr_t CanTakeDamage = 0x6F98B0; // ScheduleOne.PlayerScripts.Health.PlayerHealth$$get_CanTakeDamage
+        uintptr_t TakeDamage = 0x6F8CA0; // ScheduleOne.PlayerScripts.Health.PlayerHealth$$RpcWriter___Observers_TakeDamage_3505310624
+        uintptr_t GetStackLimit = 0x8702D0; // ScheduleOne.ItemFramework.ItemInstance$$get_StackLimit
+        uintptr_t SetStamina = 0x6DFF50; // ScheduleOne.PlayerScripts.PlayerMovement$$SetStamina
+        uintptr_t UpdateArrest = 0x65D7E0; // ScheduleOne.NPCs.Behaviour.PursuitBehaviour$$UpdateArrest
+        uintptr_t UpdateSearch = 0x627F60; // ScheduleOne.NPCs.Behaviour.BodySearchBehaviour$$UpdateSearch
+        uintptr_t AssignTarget = 0x6250F0; // ScheduleOne.NPCs.Behaviour.BodySearchBehaviour$$AssignTarget
+        uintptr_t DoesPlayerContainItemsOfInterest = 0x625E70; // ScheduleOne.NPCs.Behaviour.BodySearchBehaviour$$DoesPlayerContainItemsOfInterest
+        uintptr_t BeginBodySearch = 0x599BA0; // ScheduleOne.Police.PoliceOfficer$$BeginBodySearch
+        uintptr_t BeginBodySearch_LocalPlayer = 0x5999F0; // ScheduleOne.Police.PoliceOfficer$$BeginBodySearch.LocalPlayer
+        uintptr_t BeginBodySearch_Networked = 0x599A70; // ScheduleOne.Police.PoliceOfficer$$BeginBodySearch.Networked
+        uintptr_t PunchControllerUpdate = 0x7767C0; // ScheduleOne.Combat.PunchController$$Update
+        uintptr_t SetfieldOfView = 0x2C519B0; // UnityEngine.Camera$$set_fieldOfView
+        uintptr_t PlayerMovementUpdate = 0x6E11A0; // ScheduleOne.PlayerScripts.PlayerMovement$$Update
+        uintptr_t SkatingSkateboardUpdate = 0xA8B0A0; // ScheduleOne.Skating.Skateboard$$Update
     }
     namespace engine {
-        uintptr_t CursorsetlockState = 0x29A99F0; // UnityEngine_Cursor$$set_lockState proc near
+        uintptr_t CursorsetlockState = 0x2C73700; // UnityEngine.Cursor$$set_lockState
     }
     namespace equippable {
-        uintptr_t TrashGrabberGetCapacity = 0x84F060; // ScheduleOne_Equipping_Equippable_TrashGrabber$$GetCapacity proc near
-        uintptr_t UpdateTrashGrabber = 0x84F620; // ScheduleOne.Equipping.Equippable_TrashGrabber$$Update proc near
+        uintptr_t TrashGrabberGetCapacity = 0x86CB80; // ScheduleOne.Equipping.Equippable.TrashGrabber$$GetCapacity
+        uintptr_t UpdateTrashGrabber = 0x86D140; // ScheduleOne.Equipping.Equippable.TrashGrabber$$Update
     }
-    namespace world
-    {
-        uintptr_t SkateboardIsGrounded = 0xA63020; //ScheduleOne_Skating_Skateboard$$IsGrounded proc near
-        uintptr_t GetCookDuration = 0x99F590; // ScheduleOne.ObjectScripts.OvenCookOperation$$GetCookDuration proc near
-        uintptr_t OvenIsReady = 0x99F7E0; // ScheduleOne.ObjectScripts.OvenCookOperation$$IsReady proc near
-        uintptr_t GetRandomSymbol = 0x7881A0; // ScheduleOne.Casino.SlotMachine$$GetRandomSymbol proc near
-        uintptr_t GetCurrentBetAmount = 0x78A3C0; // ScheduleOne.Casino.SlotMachine$$get_currentBetAmount proc near
-        uintptr_t GetAdditiveGrowthMultiplier = 0x95B900; // ScheduleOne.ObjectScripts.Pot$$GetAdditiveGrowthMultiplier
+    namespace world {
+        uintptr_t SkateboardIsGrounded = 0xA894B0; // ScheduleOne.Skating.Skateboard$$IsGrounded
+        uintptr_t GetCookDuration = 0x9BF270; // ScheduleOne.ObjectScripts.OvenCookOperation$$GetCookDuration
+        uintptr_t OvenIsReady = 0x9BF4C0; // ScheduleOne.ObjectScripts.OvenCookOperation$$IsReady
+        uintptr_t GetRandomSymbol = 0x7C5250; // ScheduleOne.Casino.SlotMachine$$GetRandomSymbol
+        uintptr_t GetCurrentBetAmount = 0x7C7470; // ScheduleOne.Casino.SlotMachine$$get_currentBetAmount
+        uintptr_t GetAdditiveGrowthMultiplier = 0x97A030; // ScheduleOne.ObjectScripts.Pot$$GetAdditiveGrowthMultiplier
     }
-    namespace npc
-    {
-        uintptr_t IsCurrentlyActiveWithTolerance = 0x69DDE0; // ScheduleOne_Law_CurfewManager$$get_IsCurrentlyActiveWithTolerance proc near
-        uintptr_t GetSampleSuccess = 0x6F54F0; // ScheduleOne_Economy_Customer$$GetSampleSuccess proc near
-        uintptr_t GetOfferSuccessChance = 0x6F45C0; // ScheduleOne_Economy_Customer$$GetOfferSuccessChance proc near
-
+    namespace npc {
+        uintptr_t IsCurrentlyActiveWithTolerance = 0x6B3CD0; // ScheduleOne.Law.CurfewManager$$get_IsCurrentlyActiveWithTolerance
+        uintptr_t GetSampleSuccess = 0x71E250; // ScheduleOne.Economy.Customer$$GetSampleSuccess
+        uintptr_t GetOfferSuccessChance = 0x71D130; // ScheduleOne.Economy.Customer$$GetOfferSuccessChance
     }
 }
 namespace pointer {
@@ -60,9 +58,8 @@ namespace pointer {
     uintptr_t MaxPunchDamage = 0x34;            // ScheduleOne.Combat.PunchController
     uintptr_t jumpForce = 0x44;                 // ScheduleOne.PlayerScripts.PlayerMovement
     uintptr_t MoveSpeedMultiplier = 0x3C;       // ScheduleOne.PlayerScripts.PlayerMovement
-    uintptr_t IsGrounded = 0xBC;               // <IsGrounded>k__BackingField
+    uintptr_t IsGrounded = 0xBC;                // <IsGrounded>k__BackingField
     uintptr_t JumpForceSkating = 0x1D0;
     uintptr_t PushForceMultiplierSkating = 0x218;
     uintptr_t TopSpeed_KmhSkating = 0x214;
-
 }
